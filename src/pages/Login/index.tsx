0bfef432710eadf700fe6954f4098dc67cdd9038
Marcus Vinicius <marcusv.ribas@gmail.com>
2022-07-06 19:54:27 -0300

    adicionar footer na pagina login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,4 +1,5 @@
 import Button from "../../components/Button";
+import FooterMobile from "../../components/Footer";
 import Header from "../../components/Header";
 import Medica from "../../components/Images/Medica";
 
@@ -6,23 +7,28 @@ import Medica from "../../components/Images/Medica";
 
 export default function Login() {
   return (
-    <div className="flex flex-col justify-center items-center bg-upperImage bg-no-repeat bg-cover bg-top">
-      <Header/>
-      <Medica />
-      <h1>Lacrei Saúde para profissinais voluntários</h1>
-      <p className="text-center">
-        Melhorar a qualidade e expectativa de vida e a experiência de pessoas LGBTQIAP+ é o principal objetivo do Lacrei. Por isso, buscamos profissionais preparados para atender com segurança e qualidade na nossa
-        plataforma. Venha fazer parte!
-      </p>
-      <Button
-        className="text-white bg-verde-lacrei"
-        to="/signup"
-        text="Quero ajudar!"
-      />
-      <div className="flex">
-        <span>já tem cadastro?</span>
-        <span>acessar minha conta</span>
+    <div>
+      <div className="flex flex-col justify-center items-center bg-upperImage bg-no-repeat bg-cover bg-top">
+        <Header />
+        <Medica />
+        <h1>Lacrei Saúde para profissinais voluntários</h1>
+        <p className="text-center">
+          Melhorar a qualidade e expectativa de vida e a experiência de pessoas
+          LGBTQIAP+ é o principal objetivo do Lacrei. Por isso, buscamos
+          profissionais preparados para atender com segurança e qualidade na
+          nossa plataforma. Venha fazer parte!
+        </p>
+        <Button
+          className="text-white bg-verde-lacrei"
+          to="/signup"
+          text="Quero ajudar!"
+        />
+        <div className="flex">
+          <span>já tem cadastro?</span>
+          <span>acessar minha conta</span>
+        </div>
       </div>
+      <FooterMobile/>
     </div>
   );
 }
