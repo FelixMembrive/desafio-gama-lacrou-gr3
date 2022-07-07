f7701cceac5aa1e7b348f592cf9b8c81ae486edf
Marcus Vinicius <marcusv.ribas@gmail.com>
2022-07-07 13:01:08 -0300

    📦 NEW: create checkbox component

---
+++ src/components/Checkbox/index.tsx
@@ -0,0 +1,13 @@
+interface ICheckboxProps{
+  label: string
+}
+
+
+export default function Checkbox (props: ICheckboxProps){
+  return(
+      <div className="flex gap-4 justify-center text-sm py-3 font-nunito w-fit">
+        <input type="checkbox" id="alo" className="accent-green-500 w-5 h-5 cursor-pointer" />
+        <label htmlFor="alo" className="cursor-pointer">{props.label}</label>
+      </div>
+  )
+}
--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,9 +1,13 @@
 import { Link } from "react-router-dom";
+import Checkbox from "../../components/Checkbox";
 
 //fornece a opção de completar as informações (leva ao pós-cadastro)
 
 export default function Profile() {
     return (
-        <h1>Esta é a página de Perfil</h1>
+        <>
+            <h1>Esta é a página de Perfil</h1>
+        </>
+        
     )
 }
