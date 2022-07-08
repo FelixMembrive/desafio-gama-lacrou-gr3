2beab6b9474c14f54a25a96b23590ef7e8f197c0
FelixMembrive <felix.membrive@gmail.com>
2022-07-08 08:13:23 -0300

    ajustes na página de perfil

--- src/components/ProfilePhoto/index.tsx
+++ src/components/ProfilePhoto/index.tsx
@@ -7,6 +7,8 @@ interface IprofilePhotoProps {
 
 export default function ProfilePhoto(props: IprofilePhotoProps) {
     return (
-        <img className={""} src={profilePhoto} alt="Foto de Perfil" />
+        <div className="h-300 w-300">
+            <img className="h-86 w-86 aspect-square m-4" height={"86px"} src={profilePhoto} alt="Foto de Perfil" />
+        </div>
     )
 }
--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -40,109 +40,102 @@ export default function Profile() {
         <h1 className="font-bold text-[36px] text-center mb-8">
           Olá, {profile.name}!
         </h1>
-        <section className={"flex flex-row"}>
+        <section className={"flex flex-row items-center"}>
           <ProfilePhoto></ProfilePhoto>
-          <p className="ml-28">{profile.description}</p>
+          <div className="container">
+            <p className="w-200 mx-8">{profile.description}</p>
+          </div>
         </section>
-        <p>{profile.photoDescription}</p>
-        <button>Editar Perfil</button>
+        <p className="m-8">{profile.photoDescription}</p>
+        <button className="button">Editar Perfil</button>
         <hr className="bg-[#CFCFCF] min-w-[328px] mx-2 h-0.5 mt-6" />
         <form>
-        <CustomAccordion key={1} title="Informações pessoais">
-          <Input
-            className="mt-4"
-            labelText="Nome social completo *"
-            labelClassName="font-bold"
-            type="text"
-            inputId="input-name-profile"
-            inputClassName="min-w-[291px] mobile-md:min-w-[328px] p-4"
-            placeholder="Digite seu nome"
-            required={true}
-          />
-          <Input
-            className="mt-4"
-            labelText="Email *"
-            labelClassName="font-bold"
-            type="email"
-            inputId="input-email-profile"
-            inputClassName="min-w-[291px] mobile-md:min-w-[328px] p-4"
-            placeholder="Digite seu nome"
-            required={true}
-          />
-          <Select
-            className="mt-4"
-            labelText="Pronome e artigo"
-            inputId="select-pronouns-profile"
-            optionValues={["Ele/Dele", "Ela/Dela", "Elu/Delu", "Outro"]}
-            optionTexts={["Ele/Dele", "Ela/Dela", "Elu/Delu", "Outro"]}
-            selectClassName="min-w-[291px] mobile-md:min-w-[328px] p-4"
-          />
-          <Select
-            className="mt-4"
-            labelText="Etnia"
-            inputId="select-pronouns-profile"
-            optionValues={[
-              "Indígena",
-              "Branca",
-              "Parda",
-              "Amarela",
-              "Preta",
-              "Outra",
-            ]}
-            optionTexts={[
-              "Indígena",
-              "Branca",
-              "Parda",
-              "Amarela",
-              "Preta",
-              "Outra",
-            ]}
-            selectClassName="min-w-[291px] mobile-md:min-w-[328px] p-4"
-          />
-          <Select
-            className="mt-4"
-            labelText="Identidade de gênero"
-            inputId="select-pronouns-profile"
-            optionValues={[
-              "Intersexo",
-              "Homem Trans",
-              "Não-Binárie",
-              "Agênera",
-              "Andrógeno",
-              "Queer",
-              "Mulher Trans",
-              "Mulher Cis",
-              "Homem Cis",
-              "Outro",
-            ]}
-            optionTexts={[
-              "Intersexo",
-              "Homem Trans",
-              "Não-Binárie",
-              "Agênera",
-              "Andrógeno",
-              "Queer",
-              "Mulher Trans",
-              "Mulher Cis",
-              "Homem Cis",
-              "Outro",
-            ]}
-            selectClassName="min-w-[291px] mobile-md:min-w-[328px] p-4"
-          />
-          <Select
-            className="mt-4"
-            labelText="Sexualidade"
-            inputId="select-sexuality-profile"
-            optionValues={[
-              "Assexual",
-              "Lésbica",
-              "Heterossexual",
-              "Panssexual",
-              "Bissexual",
-              "Gay",
-              "Outra"
-            ]}
-            optionTexts={[
+          <CustomAccordion key={1} title="Informações pessoais">
+            <Input
+              className="mt-4"
+              labelText="Nome social completo *"
+              labelClassName="font-bold"
+              type="text"
+              inputId="input-name-profile"
+              inputClassName="min-w-[291px] mobile-md:min-w-[328px] p-4"
+              placeholder="Digite seu nome"
+              required={true}
+            />
+            <Input
+              className="mt-4"
+              labelText="Email *"
+              labelClassName="font-bold"
+              type="email"
+              inputId="input-email-profile"
+              inputClassName="min-w-[291px] mobile-md:min-w-[328px] p-4"
+              placeholder="Digite seu nome"
+              required={true}
+            />
+            <Select
+              className="mt-4"
+              labelText="Pronome e artigo"
+              inputId="select-pronouns-profile"
+              optionValues={["Ele/Dele", "Ela/Dela", "Elu/Delu", "Outro"]}
+              optionTexts={["Ele/Dele", "Ela/Dela", "Elu/Delu", "Outro"]}
+              selectClassName="min-w-[291px] mobile-md:min-w-[328px] p-4"
+            />
+            <Select
+              className="mt-4"
+              labelText="Etnia"
+              inputId="select-pronouns-profile"
+              optionValues={[
+                "Indígena",
+                "Branca",
+                "Parda",
+                "Amarela",
+                "Preta",
+                "Outra",
+              ]}
+              optionTexts={[
+                "Indígena",
+                "Branca",
+                "Parda",
+                "Amarela",
+                "Preta",
+                "Outra",
+              ]}
+              selectClassName="min-w-[291px] mobile-md:min-w-[328px] p-4"
+            />
+            <Select
+              className="mt-4"
+              labelText="Identidade de gênero"
+              inputId="select-pronouns-profile"
+              optionValues={[
+                "Intersexo",
+                "Homem Trans",
+                "Não-Binárie",
+                "Agênera",
+                "Andrógeno",
+                "Queer",
+                "Mulher Trans",
+                "Mulher Cis",
+                "Homem Cis",
+                "Outro",
+              ]}
+              optionTexts={[
+                "Intersexo",
+                "Homem Trans",
+                "Não-Binárie",
+                "Agênera",
+                "Andrógeno",
+                "Queer",
+                "Mulher Trans",
+                "Mulher Cis",
+                "Homem Cis",
+                "Outro",
+              ]}
+              selectClassName="min-w-[291px] mobile-md:min-w-[328px] p-4"
+            />
+            <Select
+              className="mt-4"
+              labelText="Sexualidade"
+              inputId="select-sexuality-profile"
+              optionValues={[
                 "Assexual",
                 "Lésbica",
                 "Heterossexual",
@@ -150,56 +143,65 @@ export default function Profile() {
                 "Bissexual",
                 "Gay",
                 "Outra"
-            ]}
-            selectClassName="min-w-[291px] mobile-md:min-w-[328px] p-4"
-          />
-          <Select
-            className="mt-4"
-            labelText="PCD"
-            inputId="select-disability-profile"
-            optionValues={[
-              "Deficiência motora",
-              "Deficiência cognitivo-intelectual",
-              "Deficiência auditiva",
-              "Deficiência visual",
-            ]}
-            optionTexts={[
+              ]}
+              optionTexts={[
+                "Assexual",
+                "Lésbica",
+                "Heterossexual",
+                "Panssexual",
+                "Bissexual",
+                "Gay",
+                "Outra"
+              ]}
+              selectClassName="min-w-[291px] mobile-md:min-w-[328px] p-4"
+            />
+            <Select
+              className="mt-4"
+              labelText="PCD"
+              inputId="select-disability-profile"
+              optionValues={[
                 "Deficiência motora",
                 "Deficiência cognitivo-intelectual",
                 "Deficiência auditiva",
                 "Deficiência visual",
-            ]}
-            selectClassName="min-w-[291px] mobile-md:min-w-[328px] p-4"
-          />
-        </CustomAccordion>
-        <CustomAccordion key={2} title="Informações profissionais">
-          <Input
-            className="mt-4"
-            labelText="Profissão"
-            value="Pessoa Psicóloga"
-            labelClassName="font-bold"
-            type="text"
-            inputId="input-profession-profile"
-            inputClassName="min-w-[291px] mobile-md:min-w-[328px] p-4 bg-[#CFCFCF]"
-            placeholder="Digite seu nome"
-            disabled={true}
-          />
-          <Input
-            className="mt-4"
-            labelText="Número de inscrição do conselho de classe"
-            value="CRP 08/15188"
-            labelClassName="font-bold"
-            type="text"
-            inputId="input-email-profile"
-            inputClassName="min-w-[291px] mobile-md:min-w-[328px] p-4 bg-[#CFCFCF]"
-            placeholder="Digite seu nome"
-            disabled={true}
-          />
-          <a href="/login" className="self-center mb-11 text-[#ff1192]">
-          Editar
-        </a>
-          <hr className="bg-[#CFCFCF] min-w-[328px] mx-2 h-0.5 mt-6" />
-        </CustomAccordion>
+              ]}
+              optionTexts={[
+                "Deficiência motora",
+                "Deficiência cognitivo-intelectual",
+                "Deficiência auditiva",
+                "Deficiência visual",
+              ]}
+              selectClassName="min-w-[291px] mobile-md:min-w-[328px] p-4"
+            />
+          </CustomAccordion>
+          <CustomAccordion key={2} title="Informações profissionais">
+            <Input
+              className="mt-4"
+              labelText="Profissão"
+              value="Pessoa Psicóloga"
+              labelClassName="font-bold"
+              type="text"
+              inputId="input-profession-profile"
+              inputClassName="min-w-[291px] mobile-md:min-w-[328px] p-4 bg-[#CFCFCF]"
+              placeholder="Digite seu nome"
+              disabled={true}
+            />
+            <Input
+              className="mt-4"
+              labelText="Número de inscrição do conselho de classe"
+              value="CRP 08/15188"
+              labelClassName="font-bold"
+              type="text"
+              inputId="input-email-profile"
+              inputClassName="min-w-[291px] mobile-md:min-w-[328px] p-4 bg-[#CFCFCF]"
+              placeholder="Digite seu nome"
+              disabled={true}
+            />
+            <a href="/login" className="self-center mb-11 text-[#ff1192]">
+              Editar
+            </a>
+            <hr className="bg-[#CFCFCF] min-w-[328px] mx-2 h-0.5 mt-6" />
+          </CustomAccordion>
         </form>
       </div>
       <FooterMobile />
