ddee0b1fad639f083402507ee7396f6ee90a7f35
FelixMembrive <felix.membrive@gmail.com>
2022-07-08 21:43:09 -0300

    build

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "/<REPO>/",
+  base: "/<REPO>/",
   server: {
     host: true
 
